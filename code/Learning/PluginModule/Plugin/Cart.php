4d70978a1691c6ebb36a8c617262b972a014a5ed
Deepak <deepak.padmule@afourtech.com>
2020-09-30 19:01:30 +0530

    Completed plugin development task

---
+++ code/Learning/PluginModule/Plugin/Cart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Learning\PluginModule\Plugin;
+
+class Cart
+{
+    public function afterAddProduct(\Magento\Checkout\Model\Cart $subject, $productInfo, $requestInfo=null)
+    {
+        $requestInfo['price'] = $requestInfo['price'] + 50;
+        return array($productInfo, $requestInfo);
+    }
+}
+
+?>
